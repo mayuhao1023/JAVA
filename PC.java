b6e11e1933ce29c4a5c5184fd14016d0ff3d5004
mayuhao1023 <63234129+mayuhao1023@users.noreply.github.com>
2020-10-08 22:31:16 +0800

    Add files via upload

---
+++ PC.java
@@ -0,0 +1,30 @@
+package test_two;
+
+import test_one.CPU;
+import test_one.HardDisk;
+
+public class PC {
+private HardDisk HD;
+private CPU cpu;
+public PC(){
+	
+}
+public PC(CPU cpu,HardDisk HD) {
+	setCPU(cpu);
+	setHardDisk(HD);
+}
+
+public void setCPU(CPU c) {
+	cpu=c;
+}
+void setHardDisk(HardDisk h) {
+	HD=h;
+}
+void show() {
+	System.out.println("CPU type is "+cpu.getType()+".");
+	System.out.println("CPU speed is "+cpu.getSpeed()+" MB/s.");
+	System.out.println("CPU hertz is "+cpu.getHertz()+" GHZ.");
+	System.out.println("HardDisk is "+HD.getAmount()+" G.");
+	System.out.println("HardDisk readspeed is "+HD.getReadSpeed()+" MB/s.");
+}
+}
